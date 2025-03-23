27f0b0a1e6bebb5e2abcb329822f21383611eb40
webLXM <1944754630@qq.com>
2025-03-23 17:51:42 +0800

    feat(layouts): 编写默认layouts组件

--- app.vue
+++ app.vue
@@ -2,7 +2,11 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLayout>
-      <NuxtPage />
+      <div class="wrapper">
+        <NuxtPage />
+      </div>
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+</script>
