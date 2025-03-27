00f04f94c176ee9aee986a7f560c2045d2c23ed6
webLXM <1944754630@qq.com>
2025-03-27 22:20:39 +0800

    feat(page): 自定义404页面

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+ definePageMeta({
+    layout: "empty-layout"
+ })
+</script>
+
+<template>
+  <div class="not-found">
+    <h1> 404 Not Found</h1>
+  </div>
+</template>
+
+<style scoped>
+  .not-found {
+    display: flex;
+    justify-content: center;
+  }
+</style>
